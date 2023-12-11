9ff4c39e16b8b7cb53eed335fbb804bac8f73c1c
bhanu1598 <145683672+bhanu1598@users.noreply.github.com>
2023-12-11 11:21:01 +0530

    update file

---
+++ Unicord.Universal/Controls/RichPresenceControl.xaml.cs
@@ -0,0 +1,12 @@
+using Windows.UI.Xaml.Controls;
+
+namespace Unicord.Universal.Controls
+{
+    public sealed partial class RichPresenceControl : UserControl
+    {
+        public RichPresenceControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
